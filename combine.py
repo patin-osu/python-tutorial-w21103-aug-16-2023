4fc3c231a33c82233c04c83e51c9c3cb6d5a9cea
Patin Muangjan <patin@patin.dev>
2023-08-16 13:59:37 +0700

    Done 1st place

---
+++ combine.py
@@ -0,0 +1,7 @@
+school = input("ชื่ิอโรงเรียน ")
+print(school)
+
+age = int(input("อายุโรงเรียน: "))
+print(age)
+
+print("โรงเรียน", school, "มีอายุ", age, "ปี")
---
+++ main.py
@@ -0,0 +1,12 @@
+#input()
+school = input("School name plz ")
+print(school)
+print("Your mo- i mean school name is:", school)
+
+#Int (integer = No decimal point)
+age = int(input("Age: "))
+print("Your age is:", age)
+
+#float (Float = decimal point)
+weight = float(input("Input your weight"))
+print("Your weight is:", weight, "KG")
